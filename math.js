5d312722c300cbd46a62801615c8b9034f7df10c
HaydenW <gamerwiechens35@outlook.com>
2020-09-11 16:10:16 -0700

    Added remainders to math.js

--- math.js
+++ math.js
@@ -1,1 +1,21 @@
 prompt("Please nter your number. If nothing please enter 0 ");
+
+if(remainder1 == 10){
+    remainder1 = "A";
+}
+if(remainder1 == 11){
+    remainder1 = "B";
+}
+if(remainder1 == 12){
+    remainder1 = "C";
+}
+if(remainder1 == 13){
+    remainder1 = "D";
+}
+if(remainder1 == 14){
+    remainder1 = "E";
+}
+if(remainder1 == 15){
+    remainder1 = "F";
+}
+
