3db829d0a78d34b984eaed955b6a2039c4433451
Hayden W <gamerwiechens35@outlook.com>
2020-09-14 18:27:33 -0700

    Added hexadecimal math

--- math.js
+++ math.js
@@ -2,7 +2,7 @@ let Decimal = prompt("Please enter your number. If nothing please enter 0 ");
 
 R1 = Decimal % 2;
 binary = Math.floor(Decimal / 2);
-
+//finds the remainder number
 R2 = binary % 2;
 binary = Math.floor(binary / 2);
 
@@ -24,7 +24,11 @@ binary = Math.floor(binary / 2);
 R8 = binary % 2;
 binary = Math.floor(binary / 2);
 
+console.log(`The binary of ${Decimal} is ${R1, R2, R3, R4, R5, R6, R7, R8}`);
 
+hexadec = Math.floor(Decimal / 16);
+remainder1 = Decimal % 16;
+console.log(`The hexadecimal of ${Decimal} is ${hexadec}`);
 
 if(remainder1 == 10){
     remainder1 = "A";
@@ -45,3 +49,21 @@ if(remainder1 == 15){
     remainder1 = "F";
 }
 
+if(hexadec == 10){
+     hexadec = "A";
+}
+if(hexadec == 11){
+    hexadec = "B";
+}
+if(hexadec == 12){
+    hexadec = "C";
+}
+if(hexadec == 13){
+    hexadec = "D";
+}
+if(hexadec == 14){
+    hexadec = "E";
+}
+if(hexadec == 15){
+    hexadec = "F";
+}
